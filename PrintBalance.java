ac1531a845934e05221c394dff38851e61133ec6
Demetruis Vassar <vassardm@rose-hulman.edu>
2012-04-10 20:13:00 -0400

    Update the Files

--- PrintBalance.java
+++ PrintBalance.java
@@ -1,10 +1,12 @@
+import java.text.DateFormat;
+import java.text.NumberFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.Scanner;
 
 /**
- * TODO A simple class that needs to be localized
+ * DONE A simple class that needs to be localized
  * 
  * @author mohan. Created Mar 27, 2011.
  */
@@ -15,16 +17,24 @@ public class PrintBalance {
 	 * 
 	 * @param args
 	 */
+
 	public static void main(String args[]) {
+
 		// Add in Localization
 		String language;
 		String country;
+
+		// Add in Locales
 		Locale aLocale = new Locale("en", "US");
 		Locale caLocale = new Locale("fr", "CA");
 		Locale frLocale = new Locale("fr", "FR");
+		Locale deLocale = new Locale("de", "DE");
+		Locale currentLocale = new Locale("en", "US");
+
+		// Initialize the Resource Bundle
 		ResourceBundle messages;
-		messages = ResourceBundle.getBundle("MessagesBundle", frLocale);
-		
+		messages = ResourceBundle.getBundle("MessagesBundle", aLocale);
+
 		// Initialize the Scanner
 		Scanner scanInput = new Scanner(System.in);
 		Date today = new Date();
@@ -36,15 +46,16 @@ public class PrintBalance {
 		// Get User's Name
 		System.out.println(messages.getString("username"));
 		String name = scanInput.nextLine();
-		System.out.println(messages.getString("greetings"));
+		System.out.println(messages.getString("greetings") + name);
 
 		// print today's date, balance and bid goodbye
-		// System.out.println("As of : "+ today.toString());
-		System.out.println(messages.getString("today"));
-		// System.out.println("You owe the school $9876543.21");
-		System.out.println(messages.getString("debt"));
-		// System.out.println("Good Bye");
+		System.out.println(messages.getString("today")
+				+ DateFormat.getDateTimeInstance(DateFormat.FULL,
+						DateFormat.FULL, currentLocale).format(today));
+		System.out.println(messages.getString("debt")
+				+ NumberFormat.getCurrencyInstance(currentLocale).format(
+						messages.getString("specficAmount")));
 		System.out.println(messages.getString("farewell"));
-	
+
 	}
 }
